998b482599ef1bda77e6821326c78661e6bb982f
NoName1dea <113446267+NoName1dea@users.noreply.github.com>
2023-06-02 20:48:15 +0000

    e

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String [] args) {
-        int numberOfMotors = 4;
-        int numberOfWheels = numberOfMotors + 2;
+        double numberOfMotors = 4.5;
+        double numberOfWheels = numberOfMotors + 2;
         System.out.println(numberOfWheels + " This for visubility AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 }
