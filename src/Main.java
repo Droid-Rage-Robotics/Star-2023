b23c73647ed731f7253c55c1bcc5295be84bb777
Justin <84753002+Anti-Shulk@users.noreply.github.com>
2023-06-02 20:47:34 +0000

    double code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 class Main {
     public static void main(String[] args) {
-        int a = 2322;
-        System.out.println(a);
+        int myNum = 3.3;
+        System.out.println(myNum);
     }
 }
+/*
+ * 
+ * [type] [name] = [value];
+ * 
+ * [integer types]
+ * byte - 1 byte - [-128, 127]
+ * short - 2 bytes - [-32768, 32767]
+ * int - 4 byte - [-2b, 2b]
+ * long - 8b - [-9,223,372,036,854,775,806, 
+ * 9,223,372,036,854,775,807]
+ * 
+ * [floating point numbers]
+ * float - 4 bytes
+ * double - 8 bytes
+ * 
+ * 
+ */
