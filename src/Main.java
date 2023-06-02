038587c880c6d00978ca0148fbd339f51a93a9a8
faith-4567 <112733934+faith-4567@users.noreply.github.com>
2023-06-02 20:28:14 +0000

    chage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main (String[] args  ) {
-        System.out.println("Hello World!");
+        int numberOfWheels = 4;
+        System.out.println(numberOfWheels);
+
+
     }
 }
