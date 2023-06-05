e5e2f7a8801a477bb2f335929c734c28995c04f5
Ariel <135558227+OrielTheCOOKIE@users.noreply.github.com>
2023-06-05 01:30:00 +0000

    print number of mototrs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int numberOfMotors = 4;
-        System.out.println(numberOfMotors)
+        System.out.println(numberOfMotors);
     }
 }
  
