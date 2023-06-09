7f875e0c11c6175551f2f29315227b69289ecc9e
luckys301 <91300906+luckys301@users.noreply.github.com>
2023-06-09 18:48:01 +0000

    PrintOut()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
+        printOut();
+    }
+
+    public static void printOut() {
         System.out.println("Hello World!");
 
         int a = 123;
