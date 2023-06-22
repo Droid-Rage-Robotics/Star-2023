b9494b4e84114b011ce65507d61a4a63bc046a12
Supernova1212 <132406490+Supernova1212@users.noreply.github.com>
2023-06-22 22:21:24 +0000

    questionable motor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-class Main { 
-    public static void main (String[] args ) {
-        double dos = 4.4;
-        System.out.println(dos);
-    } 
-} 
-    
+import java.util.Set;
+
+import ftc.Motor;
+import ftc.Sleep;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Motor leftMotor = new Motor(1);
+        Motor rightMotor = new Motor(0);
+        leftMotor.setPower(.50);
+        rightMotor.setPower(.5);
+        Sleep.forMilliseconds(3000);
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+    }
+}
+
+
+
